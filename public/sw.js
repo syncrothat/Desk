72c8092486c9fc7d5aaaba35ac13ebaff0fb47d1
HeroBuxx <me@herobuxx.me>
2025-01-21 14:43:35 +0700

    desk: service-worker: Apply chnages to measure latency
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,16 @@
-// service-worker.js
 self.addEventListener('push', (event) => {
-    const data = event.data.json();
-  
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: '/Desk/logo192.png',
-    });
-  });  
+  const data = event.data.json();
+
+  const timestamp = new Date(data.timestamp).toLocaleString('en-US', {
+    hour12: false,
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    fractionalSecondDigits: 3,
+  });
+
+  self.registration.showNotification(data.title, {
+    body: `${data.body} (Sent at: ${timestamp})`,
+    icon: '/Desk/logo192.png',
+  });
+});
