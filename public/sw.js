f65d394fe10203094f915c49a4ce228ccb6839c6
HeroBuxx <me@herobuxx.me>
2025-01-24 10:43:11 +0700

    desk: Remove notification timestamp
    Revert "desk: service-worker: Apply chnages to measure latency"
    
    This reverts commit 72c8092486c9fc7d5aaaba35ac13ebaff0fb47d1.

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,8 @@
 self.addEventListener('push', (event) => {
-  const data = event.data.json();
+    const data = event.data.json();
 
-  const receivedTime = new Date().toLocaleString('en-US', {
-    hour12: false,
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    fractionalSecondDigits: 3,
-  });
-
-  self.registration.showNotification(data.title, {
-    body: `${data.body} (Received at: ${receivedTime})`,
-    icon: '/Desk/logo192.png',
-  });
-});
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/Desk/logo192.png',
+    });
+  });  
